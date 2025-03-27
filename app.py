d78c6b84666c1f0e151ceca013c7e66fa7b724f6
thecountrymouth11 <thecountrymouth@gmail.com>
2025-03-27 17:23:43 +0000

    added flask web app for ec2 deployment

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Cloudops vibes activated!</h1><p>This is my Flask app running on EC2. Bow down to my automation skills.</p>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
