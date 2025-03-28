cfd9c95abc29552b5eb96dc0d69ea9354d909caf
thecountrymouth11 <thecountrymouth@gmail.com>
2025-03-28 11:06:29 +0000

    fixed syntax issue

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello():
     return '<h1>Cloudops vibes activated!</h1><p>This is my Flask app running on EC2. Bow down to my automation skills.</p>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000
+    app.run(host='0.0.0.0', port=5000)
