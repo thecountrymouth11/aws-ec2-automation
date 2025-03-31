438f6399fe50b2df3a3f17576f09d5ab361f9e2f
thecountrymouth11 <thecountrymouth@gmail.com>
2025-03-31 15:42:20 +0000

    Remove(deleted locally)

--- config.sh
+++
@@ -1,7 +0,0 @@
-# config.sh (--no shebang needed--)
-
-export AWS_REGION="us-east-1"
-export AMI_ID="ami-0e35ddab05955cf57"
-export INSTANCE_TYPE="t2.micro"
-export KEY_NAME="cloudops-key"
-export SECURITY_GROUP_ID="sg-0cdd48e63d80415b9"
